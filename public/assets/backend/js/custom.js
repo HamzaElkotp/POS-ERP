b779bbb41f4d53f5d3ffb87185a09c8bd0e4a466
Hamza Elkotb <61008779+HamzaElkotp@users.noreply.github.com>
2023-12-05 11:58:02 +0300

    some edits

--- public/assets/backend/js/custom.js
+++ public/assets/backend/js/custom.js
@@ -18,34 +18,34 @@ if(openNavParent_Btn && closeNavParent_Btn && navParent){
 
 
 
-const statistics_parent = document.getElementById("statistics-parent");
-const checkpoint_bottom = document.getElementById("checkpoint_bottom")
-const pinunpin = document.getElementById("pinunpin");
-if(statistics_parent && checkpoint_bottom){
-    window.addEventListener("scroll", function () {
-        if (window.scrollY >= statistics_parent.offsetTop) {
-            if(!statistics_parent.classList.contains("forceRelative")){
-                statistics_parent.classList.add("sticky", "cusPos");
-            } else{
-                statistics_parent.classList.remove("cusPos");
-            }
-        } else {
-            statistics_parent.classList.remove("sticky", "cusPos");
-        }
+// const statistics_parent = document.getElementById("statistics-parent");
+// const checkpoint_bottom = document.getElementById("checkpoint_bottom")
+// const pinunpin = document.getElementById("pinunpin");
+// if(statistics_parent && checkpoint_bottom){
+//     window.addEventListener("scroll", function () {
+//         if (window.scrollY >= statistics_parent.offsetTop) {
+//             if(!statistics_parent.classList.contains("forceRelative")){
+//                 statistics_parent.classList.add("sticky", "cusPos");
+//             } else{
+//                 statistics_parent.classList.remove("cusPos");
+//             }
+//         } else {
+//             statistics_parent.classList.remove("sticky", "cusPos");
+//         }
 
-        // console.log(checkpoint_bottom.offset)
-        if(window.scrollY + (statistics_parent.offsetHeight / 2.5) >= checkpoint_bottom.offsetHeight){
-            statistics_parent.classList.remove("sticky", "cusPos");
-        }
-    });     
-}
-if(pinunpin && statistics_parent){
-    pinunpin.addEventListener('click', ()=>{
-        pinunpin.classList.toggle("pinned");
-        statistics_parent.classList.toggle("forceRelative");
-        statistics_parent.classList.toggle("cusPos");
-    })
-}
+//         // console.log(checkpoint_bottom.offset)
+//         if(window.scrollY + (statistics_parent.offsetHeight / 2.5) >= checkpoint_bottom.offsetHeight){
+//             statistics_parent.classList.remove("sticky", "cusPos");
+//         }
+//     });     
+// }
+// if(pinunpin && statistics_parent){
+//     pinunpin.addEventListener('click', ()=>{
+//         pinunpin.classList.toggle("pinned");
+//         statistics_parent.classList.toggle("forceRelative");
+//         statistics_parent.classList.toggle("cusPos");
+//     })
+// }
 
 
 
